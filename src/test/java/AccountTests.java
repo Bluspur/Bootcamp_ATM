ea67b6993703da8856a61ddc0403608d6a1878f2
Craig Holman <craig-home@hotmail.com>
2023-03-24 20:09:28 +0000

    Introduced tests for an Account class to start TDD.

---
+++ src/test/java/AccountTests.java
@@ -0,0 +1,9 @@
+import org.example.Account;
+import org.junit.jupiter.api.Test;
+
+public class AccountTests {
+    @Test
+    void testInitializeAccount() {
+        Account acc = new Account();
+    }
+}
