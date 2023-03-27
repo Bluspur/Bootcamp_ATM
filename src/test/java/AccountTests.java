a9b35b42a45e45339f2bba7cac25afa2553c51f0
Craig Holman <craig-home@hotmail.com>
2023-03-27 14:23:36 +0100

    Removed early account code.

--- src/test/java/AccountTests.java
+++
@@ -1,9 +0,0 @@
-import org.example.Account;
-import org.junit.jupiter.api.Test;
-
-public class AccountTests {
-    @Test
-    void testInitializeAccount() {
-        Account acc = new Account();
-    }
-}
---
+++ src/test/java/org/example/AccountTests.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class AccountTests {
+    @Test
+    void testNewAccount() {
+        Account newAccount = new Account();
+    }
+}
