72a3a50f8d6d7105cc48011f3eb08245f6b86111
Bluspur <craig-home@hotmail.com>
2023-04-01 16:53:51 +0100

    Introduced Account class and tests. Overdraft functionality still WIP.

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -1,4 +1,36 @@
 package org.example;
-public class Account {
 
+import java.math.BigDecimal;
+
+public class Account {
+    private BigDecimal balance;
+
+    public Account(BigDecimal openingBalance) {
+        balance = openingBalance;
+    }
+
+    public Account() {
+        balance = new BigDecimal("0");
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void addFunds(BigDecimal balanceToAdd) {
+        validateValueIsNonNegative(balanceToAdd);
+
+        balance = balance.add(balanceToAdd);
+    }
+
+    public void subtractFunds(BigDecimal balanceToSubtract) {
+        validateValueIsNonNegative(balanceToSubtract);
+
+        balance = balance.subtract(balanceToSubtract);
+    }
+
+    private void validateValueIsNonNegative(BigDecimal value) {
+        if (value.signum() == -1)
+            throw new IllegalArgumentException("Value must not be negative.");
+    }
 }
--- src/test/java/org/example/AccountTests.java
+++ src/test/java/org/example/AccountTests.java
@@ -2,9 +2,74 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AccountTests {
     @Test
-    void testNewAccount() {
+    void testNewAccountBalanceShouldReturnZeroByDefault() {
         Account newAccount = new Account();
+
+        BigDecimal actualBalance = newAccount.getBalance();
+        BigDecimal expectedBalance = new BigDecimal("0");
+        assertEquals(0, expectedBalance.compareTo(actualBalance));
+    }
+
+    @Test
+    void testNewAccountBalanceShouldReturnOpeningBalance() {
+        Account newAccount = new Account(new BigDecimal("1234.56"));
+
+        BigDecimal expectedBalance = new BigDecimal("1234.56");
+        BigDecimal actualBalance = newAccount.getBalance();
+        assertEquals(0, expectedBalance.compareTo(actualBalance));
+    }
+
+    @Test
+    void testAddingMoneyToAccountShouldReturnNewBalance() {
+        Account newAccount = new Account();
+
+        BigDecimal balanceToAdd = new BigDecimal("1234.56");
+        BigDecimal expectedBalance = new BigDecimal("1234.56");
+
+        newAccount.addFunds(balanceToAdd);
+        BigDecimal actualBalance = newAccount.getBalance();
+
+        assertEquals(0, expectedBalance.compareTo(actualBalance));
+    }
+
+    @Test
+    void testSubtractingMoneyFromAccountShouldReturnNewBalance() {
+        Account newAccount = new Account(new BigDecimal("1234.56"));
+
+        BigDecimal balanceToSubtract = new BigDecimal("1234.56");
+        BigDecimal expectedBalance = new BigDecimal("0");
+
+        newAccount.subtractFunds(balanceToSubtract);
+        BigDecimal actualBalance = newAccount.getBalance();
+
+        assertEquals(0, expectedBalance.compareTo(actualBalance));
+    }
+
+    @Test
+    void testAddingNegativeFundsShouldThrowException() {
+        Account newAccount = new Account();
+        BigDecimal negativeValue = new BigDecimal("-1");
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> newAccount.addFunds(negativeValue)
+        );
+    }
+
+    @Test
+    void testSubtractingNegativeFundsShouldThrowException() {
+        Account newAccount = new Account();
+        BigDecimal negativeValue = new BigDecimal("-1");
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> newAccount.subtractFunds(negativeValue)
+        );
     }
 }
