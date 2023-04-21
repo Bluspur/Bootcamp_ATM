c5e3dfba921b40cf6bf372c37e7f2f415cdeeae8
Craig Holman <craig-home@hotmail.com>
2023-04-21 21:02:33 +0100

    Implemented basic Database for storing user and account information.

---
+++ src/main/java/org/example/data/Database.java
@@ -0,0 +1,51 @@
+package org.example.data;
+
+import org.example.accounts.Account;
+import org.example.users.Password;
+import org.example.users.User;
+import org.example.users.Username;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Database {
+    private final Map<Username, User> usernameUserMap = new HashMap<>();
+    private final Map<User, List<Account>> userAccountsMap = new HashMap<>();
+
+    public User login(Username username, Password password) throws UserNotFoundException, InvalidCredentialsException {
+        User user;
+        if (usernameUserMap.containsKey(username)) {
+            user = usernameUserMap.get(username);
+            if (user.verifyUser(username, password)) {
+                return user;
+            } else {
+                throw new InvalidCredentialsException("Incorrect Username or Password");
+            }
+        } else {
+            throw new UserNotFoundException("User not found in Database.");
+        }
+    }
+
+    public void clear() {
+        usernameUserMap.clear();
+        userAccountsMap.clear();
+    }
+
+    public User add(Username username, Password password) throws UserAlreadyExistsException {
+        if (usernameUserMap.containsKey(username))
+            throw new UserAlreadyExistsException("User with given username already exists.");
+
+        User user = new User(username, password);
+
+        usernameUserMap.put(username, user);
+        userAccountsMap.put(user, new ArrayList<>());
+
+        return user;
+    }
+
+    public List<Account> getUserAccounts(User user) {
+        return userAccountsMap.get(user);
+    }
+}
